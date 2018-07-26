a3fc842a926d5e65de8cca8faadd5ba3cba03c91
imashwani <ashwani.p30@gmail.com>
2018-07-26 15:40:18 +0530

    Create Pin for new user fixed

--- app/src/main/java/com/example/ashwani/rewardcoins/CreatePinActivity.java
+++ app/src/main/java/com/example/ashwani/rewardcoins/CreatePinActivity.java
@@ -1,10 +1,13 @@
 package com.example.ashwani.rewardcoins;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.util.ArrayMap;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,7 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.ashwani.rewardcoins.Data.CoinApi;
-import com.example.ashwani.rewardcoins.Data.EnterPinResponse;
+import com.example.ashwani.rewardcoins.Data.CreatePinResponse;
 
 import org.json.JSONObject;
 
@@ -24,6 +27,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class CreatePinActivity extends AppCompatActivity {
+    private static final String TAG = "CreatePinActivity";
     TextView error;
     EditText newPin1, newPin2;
     Button createPinBT;
@@ -38,14 +42,17 @@ public class CreatePinActivity extends AppCompatActivity {
         actionBar.setTitle("Create New Pin");
 
         initView();
-
-        mobileNo = getIntent().getStringExtra(Util.getPhonenoKey());
+        SharedPreferences sharedPreferences =
+                getSharedPreferences("com.example.app", Context.MODE_PRIVATE);
+//        token = sharedPreferences.getString(Util.getTokenKey(), "");
+        mobileNo = sharedPreferences.getString(Util.getPhonenoKey(), "");
 
         createPinBT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 error.setVisibility(View.GONE);
                 int i = checkData();
+                Log.d(TAG, "onClick: cheack data" + i);
                 if (i == 1) {
                     //TODO: call api to change password and navigate to OTP activity
 
@@ -53,16 +60,22 @@ public class CreatePinActivity extends AppCompatActivity {
                     //put something inside the map, could be null
                     jsonParams.put("mob", mobileNo);
                     jsonParams.put("pin", newPin1.getText().toString());
+                    Log.d(TAG, "onClick: json sending data : " + jsonParams);
 
                     RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"), (new JSONObject(jsonParams)).toString());
                     //serviceCaller is the interface initialized with retrofit.create...
-                    Call<EnterPinResponse> response = CoinApi.getCoinService().enterPinCall(body);
+                    Call<CreatePinResponse> response = CoinApi.getCoinService().createPinCall(body);
 
-                    response.enqueue(new Callback<EnterPinResponse>() {
+                    response.enqueue(new Callback<CreatePinResponse>() {
                         @Override
-                        public void onResponse(Call<EnterPinResponse> call, Response<EnterPinResponse> response) {
-                            if (response.body().getSuccess() == true) {
+                        public void onResponse(Call<CreatePinResponse> call, Response<CreatePinResponse> response) {
+                            if (response == null) {
+                                Toast.makeText(CreatePinActivity.this,
+                                        "Null response from server", Toast.LENGTH_SHORT).show();
+                            } else if (response.body().getSuccess() == true) {
                                 Toast.makeText(CreatePinActivity.this, "New Pin has been created", Toast.LENGTH_SHORT).show();
+                                Log.d(TAG, "onResponse: msg from server on success" + response.body().getMsg());
+
                                 Intent intent = new Intent(CreatePinActivity.this, MainActivity.class);
                                 startActivity(intent);
                                 finish();
@@ -73,8 +86,7 @@ public class CreatePinActivity extends AppCompatActivity {
                         }
 
                         @Override
-                        public void onFailure(Call<EnterPinResponse> call, Throwable t) {
-
+                        public void onFailure(Call<CreatePinResponse> call, Throwable t) {
                         }
                     });
 
@@ -83,11 +95,11 @@ public class CreatePinActivity extends AppCompatActivity {
                 } else if (i == 2) {
                     showError("new entered pins don't match!");
                 }
-
             }
         });
 
     }
+
 
     private int checkData() {
         //1 represents all things are correct and good to go
@@ -100,11 +112,10 @@ public class CreatePinActivity extends AppCompatActivity {
             return b;
         }
 
-        if (!newPin1.equals(newPin2)) {
+        if (!new1.equals(new2)) {
             b = 2;//2 represents new entered pins don't match
             return b;
         }
-
 
         return b;
     }
@@ -116,9 +127,9 @@ public class CreatePinActivity extends AppCompatActivity {
 
     private void initView() {
 
-        newPin1 = findViewById(R.id.new_pin1);
-        newPin2 = findViewById(R.id.new_pin2);
-        createPinBT = findViewById(R.id.bt_change_pin);
+        newPin1 = findViewById(R.id.create_new_pin_et);
+        newPin2 = findViewById(R.id.create_confirm_pin_et);
+        createPinBT = findViewById(R.id.bt_create_pin);
         error = findViewById(R.id.error_create_pin);
 
         error.setVisibility(View.GONE);
